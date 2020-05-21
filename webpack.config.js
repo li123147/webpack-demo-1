d0fcdb0152857346b6a7ebba3331b1e941bd2a2e
lixiaohu <986391084@qq.com>
2020-05-21 18:23:45 +0800

    保存

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require('path');
 
 module.exports = {
-    mode: 'development',
-    //mode:切换模式
-    entry: './src/index.js',
-    //entry:入口
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js'
-        //filename:出口
-    }
+  mode: "development", //mode:切换模式
+  entry: "./src/index.js", //entry:入口
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[contenthash].js", //filename:出口
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "新垣结衣",
+      template: "src/assets/index.html"
+    }),
+      module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  ],
 };
